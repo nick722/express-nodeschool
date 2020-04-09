14a10c5adf3e3051d63fe1116192136ca0d3a101
Nikolai Iakushchenko <Nikolai_Iakushchenko@epam.com>
2020-04-09 17:35:59 +0300

    4 GOOD OLD FORM

--- src/Express.js
+++ src/Express.js
@@ -1,13 +1,12 @@
 const express = require("express");
-const path = require("path");
+const bodyparser = require("body-parser");
 
 const app = express();
 
-app.set("view engine", "pug");
-app.set("views", path.join(__dirname, "templates"));
+app.use(bodyparser.urlencoded({ extended: false }));
 
-app.use("/home", (req, res) => {
-  res.render(process.argv[3], { date: new Date().toDateString() });
+app.post("/form", (req, res) => {
+  res.end(req.body.str.split("").reverse().join(""));
 });
 
 app.listen(process.argv[2]);
