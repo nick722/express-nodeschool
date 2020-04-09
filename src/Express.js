2245e6ea8a444eaf3038f1d4d9358a5bab65841d
Nikolai Iakushchenko <Nikolai_Iakushchenko@epam.com>
2020-04-09 16:53:27 +0300

    2 STATIC

--- src/Express.js
+++ src/Express.js
@@ -1,18 +1,7 @@
-// Create an Express.js app that outputs "Hello World!" when somebody goes to /home.
-//
-//     The port number will be provided to you by expressworks as the first argument of
-// the application, i.e., process.argv[2].
-//
-//     Run $ killall node  before verifying exercises (in your terminal on Mac OS X) to end any previous processes.
-//
-//     For Windows, use "taskkill /IM node.exe" in Command Prompt.
-
 const express = require("express");
 
 const app = express();
 
-app.get("/home", (req, res) => {
-  res.end("Hello World!");
-});
+app.use(express.static(process.argv[3]));
 
 app.listen(process.argv[2]);
