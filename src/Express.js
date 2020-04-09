6962ee81dda27d8a1db3c4bc5d46ed1f4c5791f2
Nikolai Iakushchenko <Nikolai_Iakushchenko@epam.com>
2020-04-09 17:07:51 +0300

    3 PUG

--- src/Express.js
+++ src/Express.js
@@ -1,7 +1,13 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
-app.use(express.static(process.argv[3]));
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "templates"));
+
+app.use("/home", (req, res) => {
+  res.render(process.argv[3], { date: new Date().toDateString() });
+});
 
 app.listen(process.argv[2]);
